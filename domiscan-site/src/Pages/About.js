1bf4bab15b84fa837dc2cec2a87d0bb207ba3b58
Vilarj <lamaravillasan@outlook.com>
2022-07-07 13:14:19 -0400

    Filling up some data about the site.

--- domiscan-site/src/Pages/About.js
+++ domiscan-site/src/Pages/About.js
@@ -5,6 +5,24 @@ export default function Contact() {
     <div>
       <Header />
       <h1>About Us</h1>
+
+      <div>
+        <h3>Carlos Daniel Vilar Jimenez</h3>
+        <p>Wentworth Institute of Technology (WIT) student studying Computer Science</p>
+        <a href="https://github.com/vilarj/" target="blank">Github</a>
+      </div>
+
+      <div>
+        <h3>Luis Vargas</h3>
+        <p>Wentworth Institute of Technology (WIT) student studying Computer Science</p>
+        <a href="https://github.com/vargasl4atwit" target="blank">Github</a>
+      </div>
+
+      <div>
+        <h3>Tyler Stevens</h3>
+        <p>Wentworth Institute of Technology (WIT) student studying Computer Science</p>
+        <a href="https://github.com/tstevens728" target="blank">Github</a>
+      </div>
     </div>
   );
 }
--- domiscan-site/src/Pages/Contact.js
+++ domiscan-site/src/Pages/Contact.js
@@ -4,7 +4,29 @@ const Contact = () => {
   return (
     <div>
       <Header />
-      <h1>Contact Me</h1>
+      <h1>Contact Us</h1>
+
+      <div className="form-container">
+        <form action="">
+          <label for="fname">First Name</label>
+          <input type="text" id="fname" name="firstname" placeholder="Domi"></input>
+
+          <label for="fname">Last Name</label>
+          <input type="text" id="lname" name="lastname" placeholder="Scan"></input>
+
+          <label for="player">DomiScan User?</label>
+          <select id="player" name="player">
+            <option value="na">N/A</option>
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+
+          <label>Send message</label>
+          <textarea placeholder="Type something..."></textarea>
+
+
+        </form>
+      </div>
     </div>
   );
 };
